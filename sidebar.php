fa3a6374dfcaecaef0530ba35b87db05c717eb5e
Issac Hutchison <issac@Issacs-MacBook-Pro.local>
2020-01-20 12:38:22 -0500

    commit

--- sidebar.php
+++ sidebar.php
@@ -11,7 +11,9 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
-
+<link rel="stylesheet" href="style.css">
+<div class = "align-right">
 <aside id="secondary" class="widget-area">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
+</div>
