3171d00af6e2faba67a9f4f05ede3351e1873f68
Issac Hutchison <issac@Issacs-MacBook-Pro.local>
2020-01-21 08:07:26 -0500

    commit

--- header.php
+++ header.php
@@ -17,10 +17,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="stylesheet" href="style.css">
+		
 
 	<?php wp_head(); ?>
 </head>
-<div class = "align-center">	
+
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'underscores' ); ?></a>
@@ -55,5 +56,5 @@
 			?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
-</div>
+	</div>
 	<div id="content" class="site-content">
