ed17a94160c6086e475a8fb3a2b65c8c4a7f42a2
Issac Hutchison <issac@Issacs-MacBook-Pro.local>
2020-01-23 21:18:37 -0500

    commit

--- 404.php
+++ 404.php
@@ -19,29 +19,14 @@ get_header();
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'underscores' ); ?></p>
+					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'underscores' ); ?></p>
 
 					<?php
 					get_search_form();
 
 					the_widget( 'WP_Widget_Recent_Posts' );
 					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'underscores' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
+					<img class= "img" src="<?php bloginfo ('Wordpress-Theme'); ?>/assets/images/HTML-Yeti-404-Page.png">
 					<?php
 					/* translators: %1$s: smiley */
 					$underscores_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'underscores' ), convert_smilies( ':)' ) ) . '</p>';
