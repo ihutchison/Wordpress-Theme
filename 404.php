5465ab27dcbb31529582f3254b1e336ed8fed564
Issac Hutchison <issac@Issacs-MacBook-Pro.local>
2020-01-23 21:54:38 -0500

    commit

--- 404.php
+++ 404.php
@@ -25,7 +25,7 @@ get_header();
 					<?php
 					get_search_form();
 					?>
-					<img src="wp-content\themes\wordpress-theme\images\HTML-Yeti-404-Page.png" alt="" height="300" width="300" align="center">
+					<img src="wp-content\themes\wordpress-theme\images\HTML-Yeti-404-Page.png" alt="A Yeti 404 Comic" height="300" width="300" align="center">
 					<?php
 					/* translators: %1$s: smiley */
 					$underscores_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'underscores' ), convert_smilies( ':)' ) ) . '</p>';
